38bdd858d617acc2bf275f54c996773bb2f3ae95
ulfahrsdn <ulfahrosdiana95@gmail.com>
2026-01-18 21:13:20 +0700

    Update Churn.py

--- Churn.py
+++ Churn.py
@@ -24,7 +24,7 @@ st.set_page_config(
 CUSTOM_CSS = """
 <style>
 .stApp {
-    background-color: #d1a162; /* solid cream muda */
+    background-color: #d4b792; /* solid cream muda */
     color: #00796b; /* default teks */
 }
 [data-testid="stSidebar"] {
